eb2d483ead695da2bc04c3c17d340ecd7ea21f59
cinnamo00n <vanessa.schneeweiss@hak-feldkirch.at>
2026-03-27 22:46:33 +0100

    Button sounds
    Button sound, Setting funktioniert, Start und Quit net aber ig es ist weil quit halt das spiel schließt und start zu einer neuen szene geht

--- Assets/Audiocontroll.cs
+++ Assets/Audiocontroll.cs
@@ -5,15 +5,15 @@ using UnityEngine.UI;
 using UnityEngine.UIElements.Experimental;
 public class audiocontroll : MonoBehaviour
 {
-  public AudioMixer audiomixer;
-    public Slider musicSlider;
+  public AudioMixer audiomixer;             // Reference to the AudioMixer
+    public Slider musicSlider;              // Reference to the UI Slider for music volume
     void Start()
     {
-        musicSlider.onValueChanged.AddListener(SetVolume);
+        musicSlider.onValueChanged.AddListener(SetVolume);     // Add listener to the slider to call SetVolume when the value changes
     }
-    public void SetVolume(float value)
+    public void SetVolume(float value)            // Method to set the volume based on the slider value
     {
         if (value <= 0) value  = 0.0001f; // Avoid log of zero
-        audiomixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+        audiomixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);     // Convert slider value to decibels and set it in the AudioMixer
     }
 }
